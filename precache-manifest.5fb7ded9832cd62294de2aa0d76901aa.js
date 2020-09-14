3e315ee66bd2aef70696e620326f9c9e5d72e491
Андрей <gmgm.ab@gmail.com>
2020-09-14 13:47:30 +0700

    Updates

---
+++ precache-manifest.5fb7ded9832cd62294de2aa0d76901aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91ad980908fc7153d8292c56ec5077c4",
+    "url": "/sortable-list-app/index.html"
+  },
+  {
+    "revision": "802be8385e9a6f250d21",
+    "url": "/sortable-list-app/static/css/main.22e356ac.chunk.css"
+  },
+  {
+    "revision": "7d2b6e45d209125a0910",
+    "url": "/sortable-list-app/static/js/2.d3eec6fa.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/sortable-list-app/static/js/2.d3eec6fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "802be8385e9a6f250d21",
+    "url": "/sortable-list-app/static/js/main.c77aed3b.chunk.js"
+  },
+  {
+    "revision": "d91c42591b36f1e474ab",
+    "url": "/sortable-list-app/static/js/runtime-main.06aa31e9.js"
+  }
+]);
